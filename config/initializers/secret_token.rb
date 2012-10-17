3a0ab2e1ce135b5f0c50b4a946fa6d4b1a1d0cbd
Theodor Vararu <theo@tvararu.ro>
2012-10-17 18:37:58 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doughnut::Application.config.secret_token = 'e14792338e89f515d98e45482616c438de513de85173557b836effa09a3d2cb43add574d76efc3a6bbae74609b06918d914828be232be42b03f3e9ce90398ea7'
