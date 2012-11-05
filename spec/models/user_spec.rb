db15f26f01022ae52b8ae9ae7509a7cfd4065065
Theodor Vararu <theo@tvararu.ro>
2012-11-05 14:15:06 +0200

    Refactor some tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,99 +8,117 @@ describe User do
   
   subject { @user }
   
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:remember_token) }
-  it { should respond_to(:admin) }
-  it { should respond_to(:authenticate) }
+  describe "attributes" do
+    it { should respond_to(:name) }
+    it { should respond_to(:email) }
+    it { should respond_to(:password_digest) }
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
+    it { should respond_to(:admin) }
+    it { should respond_to(:authenticate) }
+
+    it { should be_valid }
+    it { should_not be_admin }
+  end
   
-  it { should be_valid }
-  it { should_not be_admin }
-  
-  describe "accessible attributes" do
-    it "should not allow access to admin" do
+  describe "admin" do
+    it "should not be accessible" do
       expect do
         User.new(admin: true)
       end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
     end
+    
+    describe "set to 'true'" do
+      before do
+        @user.save!
+        @user.toggle!(:admin)
+      end
+
+      it { should be_admin }
+    end
   end
   
-  describe "with admin attribute set to 'true'" do
-    before do
-      @user.save!
-      @user.toggle!(:admin)
+  describe "name" do
+    describe "not present" do
+      before { @user.name = " " }
+      it { should_not be_valid }
     end
     
-    it { should be_admin }
-  end
-  
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
-  
-  describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
-  end
-  
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
-  end
-  
-  describe "when email format is invalid" do
-    it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.should_not be_valid
-      end      
+    describe "too long" do
+      before { @user.name = "a" * 51 }
+      it { should_not be_valid }
     end
   end
-
-  describe "when email format is valid" do
-    it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        @user.should be_valid
-      end      
-    end
-  end
-
-  describe "when email address is already taken" do
-    before do
-      duplicate_user = @user.dup
-      duplicate_user.email = @user.email.upcase
-      duplicate_user.save
+  
+  describe "email" do
+    describe "not present" do
+      before { @user.email = " " }
+      it { should_not be_valid }
     end
     
-    it { should_not be_valid }
-  end
+    describe "format is invalid" do
+      it "should be invalid" do
+        addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                       foo@bar_baz.com foo@bar+baz.com]
+        addresses.each do |invalid_address|
+          @user.email = invalid_address
+          @user.should_not be_valid
+        end
+      end
+    end
+    
+    describe "format is valid" do
+      it "should be valid" do
+        addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+        addresses.each do |valid_address|
+          @user.email = valid_address
+          @user.should be_valid
+        end
+      end
+    end
+    
+    describe "is taken" do
+      before do
+        duplicate_user = @user.dup
+        duplicate_user.email = @user.email.upcase
+        duplicate_user.save
+      end
 
-  describe "when password is not present" do
-    before { @user.password = @user.password_confirmation = " " }
-    it { should_not be_valid }
+      it { should_not be_valid }
+    end
+    
+    describe "is mixed case" do
+      let(:mixed_case_email) { "Foo@ExAMPle.com" }
+
+      it "should be saved as all lower-case" do
+        @user.email = mixed_case_email
+        @user.save
+        @user.reload.email.should == mixed_case_email.downcase
+      end
+    end
   end
   
-  describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
-    it { should_not be_valid }
-  end
-  
-  describe "when password confirmation confirmation is nil" do
-    before { @user.password_confirmation = nil }
-    it { should_not be_valid }
-  end
+  describe "password" do
+    describe "not present" do
+      before { @user.password = @user.password_confirmation = " " }
+      it { should_not be_valid }
+    end
 
-  describe "when password is too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
+    describe "too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5 }
+      it { should be_invalid }
+    end
+    
+    describe "doesn't match confirmation" do
+      before { @user.password_confirmation = "mismatch" }
+      it { should_not be_valid }
+    end
+    
+    describe "confirmation is nil" do
+      before { @user.password_confirmation = nil }
+      it { should_not be_valid }
+    end
   end
   
   describe "return value of authenticate method" do
@@ -116,16 +134,6 @@ describe User do
       
       it { should_not == user_with_invalid_password }
       specify { user_with_invalid_password.should be_false }
-    end
-  end
-
-  describe "when email address is mixed case" do
-    let(:mixed_case_email) { "Foo@ExAMPle.com" }
-    
-    it "should be saved as all lower-case" do
-      @user.email = mixed_case_email
-      @user.save
-      @user.reload.email.should == mixed_case_email.downcase
     end
   end
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,81 +7,46 @@ describe "UserPages" do
   describe "index" do
     let(:admin) { FactoryGirl.create(:admin) }
     
-    before(:each) do
+    before do
       sign_in admin
       visit users_path
     end
     
-    describe "should work as admin" do
+    describe "page" do
       it { should have_selector('title', text: 'All users') }
       it { should have_selector('h1', text: 'All users')}
-    end
-    
-    describe "pagination" do
+      
       before(:all) { 30.times { FactoryGirl.create(:user) } }
       after(:all) { User.delete_all }
       
-      it { should have_selector('div.pagination') }
-      
-      it "should list each user" do
-        User.paginate(page: 1).each do |user|
-          page.should have_selector('li', text: user.name)
+      describe "pagination" do
+
+        it { should have_selector('div.pagination') }
+
+        it "should list each user" do
+          User.paginate(page: 1).each do |user|
+            page.should have_selector('li', text: user.name)
+          end
         end
       end
-    end
       
-    describe "delete links" do
-      it { should have_link('delete', href: user_path(User.first)) }
-      it "should be able to delete another user" do
-        expect { click_link('delete') }.to change(User, :count).by(-1)
-      end
-      it { should_not have_link('delete', href: user_path(admin)) }
-    end
-  end
-  
-  describe "signup page" do
-    before { visit signup_path }
-  
-    it { should have_selector('h1', text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
-  end
-
-  describe "profile page" do
-    let (:user) { FactoryGirl.create(:user) }
-    
-    describe "as own user" do
-      before do
-        sign_in user
-        visit user_path(user)
-      end
-    
-      describe "should show the profile page" do
-        it { should have_selector('h1', text: user.name) }
-        it { should have_selector('title', text: user.name)}
-      end
-    end
-    
-    describe "as a different user" do
-      let (:bill) { FactoryGirl.create(:user) }
-      
-      before { visit user_path(bill) }
-      
-      describe "should not show somebody else's profile page" do
-        it { should_not have_selector('h1', text: bill.name) }
-      end
-    end
-
-    describe "as a guest" do
-      before { visit user_path(user) }
-
-      describe "should not show somebody's profile page" do
-        it { should_not have_selector('h1', text: user.name) }
+      describe "delete links" do
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
   end
 
   describe "signup" do
     before { visit signup_path }
+    
+    describe "page" do
+      it { should have_selector('h1', text: 'Sign up') }
+      it { should have_selector('title', text: full_title('Sign up')) }
+    end
     
     let(:submit) { "Create my account" }
     
@@ -117,6 +82,40 @@ describe "UserPages" do
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         it { should have_link('Sign out') }
+      end
+    end
+  end
+
+  describe "profile page" do
+    let (:user) { FactoryGirl.create(:user) }
+    
+    describe "as own user" do
+      before do
+        sign_in user
+        visit user_path(user)
+      end
+    
+      describe "should show the profile page" do
+        it { should have_selector('h1', text: user.name) }
+        it { should have_selector('title', text: user.name)}
+      end
+    end
+    
+    describe "as a different user" do
+      let (:bill) { FactoryGirl.create(:user) }
+      
+      before { visit user_path(bill) }
+      
+      describe "should not show somebody else's profile page" do
+        it { should_not have_selector('h1', text: bill.name) }
+      end
+    end
+
+    describe "as a guest" do
+      before { visit user_path(user) }
+
+      describe "should not show somebody's profile page" do
+        it { should_not have_selector('h1', text: user.name) }
       end
     end
   end
