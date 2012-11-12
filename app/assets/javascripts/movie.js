66b179c20821ab65af8200c2712a6a7619773755
Theodor Vararu <theo@tvararu.ro>
2012-11-12 17:57:47 +0200

    Work on the landing page animation.

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -1,26 +1,58 @@
-var textContainer = new Group()
+var youWon = new Text('CONGRATULATIONS!')
 	.attr({
-		y: -200
-	});;
+		
+	});
 
-var txt = new Text('Silly rabbit.').addTo(textContainer)
+var silly = new Text('Don\'t be silly.')
 	.attr({
+		y: -200,
 		fontFamily: '\"Helvetica Neue\", Helvetica, Arial, sans-serif',
-		fontSize: '200px',
+		fontSize: '150px',
+		textFillColor: '#333'
+	});
+	
+var free = new Text("DOUGHNUT is free.")
+	.attr({
+		x: 1170,
+		y: 150,
+		width: 1170,
+		fontFamily: '\"Helvetica Neue\", Helvetica, Arial, sans-serif',
+		fontSize: '100px',
 		textFillColor: '#333'
 	});
 
 stage.on('click', function(e) {	
-	textContainer.addTo(stage);
-	// new KeyframeAnimation('1s', {
-	//    '0s' : { x: 0, y: 0 },
-	//   '.5s' : { x: 50, y: 50 },
-	//    '1s' : { x: 150, y: 100 }
-	// });
-	textContainer.animate('1s', {
-		y: 100
+	silly.addTo(stage);
+	silly.animate('1s', {
+		y: 150
 	}, {
 		easing: 'elasticOut',
-		delay: '200ms'
+		delay: '200ms',
+onEnd: function() {
+	free.addTo(stage);
+	silly.animate('300ms', {
+		x: -1170
+	}, {
+		easing: 'backIn',
+		delay: '500ms'
 	});
+	free.animate('500ms', {
+		x: 100
+	}, {
+		easing: 'backOut',
+		delay: '800ms',
+	});
+}
+	}
+	);
+	// textContainer.animate('1s', {
+	// 	y: 100
+	// }, {
+	// 	easing: 'elasticOut',
+	// 	delay: '200ms'
+	// }, {
+	// onEnd: function() {
+	// textContainer.animate('2s', { y: 300 })
+	// }
+	// });
 });
