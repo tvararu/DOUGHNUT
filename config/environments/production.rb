c7a09c5e50df18582e26b29c7680d79fbbfa9515
Theodor Vararu <theo@tvararu.ro>
2012-11-14 02:45:48 +0200

    Have to temporarily turn off SSL because Chrome won't load bonsai otherwise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Doughnut::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
@@ -28,7 +28,7 @@ Doughnut::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
